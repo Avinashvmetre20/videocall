b7fd0e29e1aab8988b7de90ce0d8143274504730
Avinashvmetre20 <avinashvmetre20@gmail.com>
2025-06-26 15:02:39 +0530

    added

--- public/script.js
+++ public/script.js
@@ -2,6 +2,8 @@ const socket = io();
 
 let localStream;
 let peerConnection;
+let targetSocketId = "";
+
 const servers = {
   iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
 };
@@ -9,15 +11,39 @@ const servers = {
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
 const targetInput = document.getElementById('targetId');
+const callBtn = document.getElementById('callBtn');
+const myIdDisplay = document.getElementById('myId');
+const usersList = document.getElementById('users');
 
-// Get local stream
+// Show your socket ID
+socket.on("connect", () => {
+  myIdDisplay.innerText = socket.id;
+});
+
+// Show online users
+socket.on("online-users", (userList) => {
+  usersList.innerHTML = "";
+  userList.forEach(id => {
+    if (id !== socket.id) {
+      const li = document.createElement("li");
+      li.innerText = id;
+      li.onclick = () => {
+        targetInput.value = id;
+      };
+      usersList.appendChild(li);
+    }
+  });
+});
+
+// Get user media
 navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-  .then((stream) => {
+  .then(stream => {
     localStream = stream;
     localVideo.srcObject = stream;
   })
   .catch(console.error);
 
+// Receive offer
 socket.on('offer', async ({ offer, from }) => {
   createPeerConnection(from);
 
@@ -28,16 +54,22 @@ socket.on('offer', async ({ offer, from }) => {
   socket.emit('answer', { answer, to: from });
 });
 
+// Receive answer
 socket.on('answer', async ({ answer }) => {
   await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
 });
 
+// Receive ICE candidates
 socket.on('candidate', async ({ candidate }) => {
   if (peerConnection) {
     await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
   }
 });
 
+// Call button click
+callBtn.addEventListener('click', startCall);
+
+// Create peer connection
 function createPeerConnection(targetId) {
   peerConnection = new RTCPeerConnection(servers);
 
@@ -51,13 +83,16 @@ function createPeerConnection(targetId) {
     remoteVideo.srcObject = event.streams[0];
   };
 
-  localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+  localStream.getTracks().forEach(track => {
+    peerConnection.addTrack(track, localStream);
+  });
 }
 
+// Start call
 async function startCall() {
   const targetId = targetInput.value.trim();
   if (!targetId) {
-    return alert('Please enter a target socket ID');
+    return alert('Please enter a valid target socket ID');
   }
 
   createPeerConnection(targetId);
--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const io = require('socket.io')(http);
 
 app.use(express.static('public'));
 
+const users = new Map();
+
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
+  users.set(socket.id, socket);
+
+  // Send updated list of online users
+  io.emit('online-users', Array.from(users.keys()));
 
   socket.on('offer', (data) => {
     io.to(data.to).emit('offer', { offer: data.offer, from: socket.id });
@@ -22,10 +28,15 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
+    users.delete(socket.id);
+    io.emit('online-users', Array.from(users.keys()));
   });
 });
 
-const PORT = 8080;
+// Optional: prevent favicon 404 error
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+const PORT =8080;
 http.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
